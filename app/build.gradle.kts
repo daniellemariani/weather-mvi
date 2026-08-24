c0515faa8fa92ad768bd6cb04a2e63c84e2756c5
Danielle Mariani <danielle.mariani@gmail.com>
2026-08-24 16:05:45 -0700

    Update compileSdk version and add Lifecycle/Coroutine dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,11 +4,7 @@ plugins {
 
 android {
     namespace = "com.dmariani.weathermvi"
-    compileSdk {
-        version = release(36) {
-            minorApiLevel = 1
-        }
-    }
+    compileSdk = 37
 
     defaultConfig {
         applicationId = "com.dmariani.weathermvi"
@@ -37,6 +33,14 @@ dependencies {
     implementation(libs.androidx.appcompat)
     implementation(libs.androidx.core.ktx)
     implementation(libs.material)
+
+    // Lifecycle
+    implementation(libs.androidx.lifecycle.runtime.ktx)
+
+    // Coroutines
+    implementation(libs.kotlinx.coroutines.core)
+    implementation(libs.kotlinx.coroutines.android)
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.espresso.core)
     androidTestImplementation(libs.androidx.junit)
