3698b5600f34d1d7cd8673558276969eca750333
Danielle Mariani <danielle.mariani@gmail.com>
2026-08-28 12:15:31 -0700

    Add UI Theme

---
+++ app/src/main/java/com/dmariani/weathermvi/ui/theme/Color.kt
@@ -0,0 +1,11 @@
+package com.dmariani.weathermvi.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val SkyBlue80 = Color(0xFFB3D9FF)
+val SkyBlueGrey80 = Color(0xFFC2CFDB)
+val Sunset80 = Color(0xFFF3CCB2)
+
+val SkyBlue40 = Color(0xFF3A6EA5)
+val SkyBlueGrey40 = Color(0xFF556270)
+val Sunset40 = Color(0xFFA65E3A)
